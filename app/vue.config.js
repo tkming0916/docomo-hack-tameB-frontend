f73ef780d7033d97c73271659c553c13594c2007
tkming0916 <iguchi.takumi@g.wakayama-u.jp>
2023-08-30 19:07:10 +0900

    fix:ポート番号問題解決

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer: {
+    port: 8081, // 任意のポート番号に変更
+  },
+});
