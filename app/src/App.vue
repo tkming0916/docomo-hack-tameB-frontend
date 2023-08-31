880218a5b66433ba758e559e0c2992b993e02b45
{kaneko10} <{jinziyounan886@gmail.com}>
2023-08-31 22:39:18 +0900

    チャットページにTailwind CSSを適用
    Co-authored-by: Reo-Sato <Reo-Satooooo@users.noreply.github.com>

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <div id="app">
+    <PageHeader />
     <router-view></router-view>
   </div>
 </template>
+
+<script>
+import PageHeader from './components/PageHeader.vue'; // Headerコンポーネントをインポート
+
+export default {
+  name: 'App',
+  components: {
+    PageHeader // コンポーネントを登録
+  },
+};
+</script>
---
+++ app/src/components/PageHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-gray-800 text-white p-4">
+    <h1 class="text-2xl font-bold">共通ヘッダー</h1>
+    <!-- ここに共通のヘッダー要素を配置 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+/* ここに共通のヘッダーのスタイルを記述 */
+</style>
