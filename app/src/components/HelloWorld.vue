b6c90c2d5fd6c10f00754ac9a50b2f88b2ab485a
tkming0916 <iguchi.takumi@g.wakayama-u.jp>
2023-08-31 15:57:07 +0900

    add:カーシェア一覧のページ

--- app/src/components/HelloWorld.vue
+++ app/src/components/HelloWorld.vue
@@ -1,67 +1,43 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="app">
+    <header class="bg-blue-500 text-white p-4">
+      <h1 class="text-2xl font-semibold">カーシェア一覧</h1>
+    </header>
 
-
-    <hello-child message=''/>
-
+    <main id="main_content" class="l-mainContent l-article bg-gray-100 p-8">
+      <div>
+        <Card_car v-for="card in cards" :key="card.id" :cardData="card" />
+      </div>
+      
+    </main>
   </div>
 </template>
 
 <script>
-import HelloChild from './HelloChild'
+import Card_car from './card_car.vue';
+import cardsData from '../assets/cars.json'; // JSONファイルのパスに合わせて調整
 
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
   components: {
-    HelloChild
+    Card_car
+  },
+  data() {
+    return {
+      // showCars: false,
+      cards: cardsData
+    };
+  },
+  methods: {
+    buttonClicked(){
+      window.alert('クリックしましたね')
+    },
+    showDetails(car) {
+      this.selectedCar = car === this.selectedCar ? null : car;
+    }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+/* ... 必要なスタイルをここに追加 ... */
 </style>
